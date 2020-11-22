d4dd80f6a6b5a874a7b2dd648ba62eeeef6936d8
Miguel Santos <mig@migsan.com>
2020-11-22 02:26:46 +0000

    openapi place holders

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,7 @@ import (
 
 // Routes - api end points
 func Routes(router *gin.Engine) {
-	// swagger:route GET /debts debts
+	// swagger:route GET /debts debts GetAllDebts
 	//
 	// Lists debts
 	//
@@ -47,14 +47,62 @@ func Routes(router *gin.Engine) {
 	//			content:
 	//				$ref: #/responses/validationError
 	router.GET("/debts", api.GetAllDebts)
+
+	// swagger:route POST /debts debts AddDebt
+	//
+	// Adds a debt
+	//
+	// Add a debt
+	//
+	// 	Consumes:
+	//  	- application/json
+	//    - application/x-protobuf
+	//
+	//	Produces:
+	//  	- application/json
+	//    - application/x-protobuf
+	//
+	//	Schemes: http, https, ws, wss
+	//
+	//	requestBody:
+	//		description: a debt
+	//		required: true
+	//		content:
+	//			application/json:
+	//				schema:
+	//					$ref: #/definitions/Debt
+	//
+	//	responses:
+	//		200:
+	//			description: An array of debts
+	//			content:
+	//				application/json:
+	//					schema:
+	//						type: Object
+	//						items:
+	//							$ref: #/definitions/Debt
+	//		400:
+	//			description: Bad user input
+	//			content:
+	//				$ref: #/responses/validationError
 	router.POST("/debts", api.AddDebt)
+
+	// swagger:route GET /debts/:debtId debts GetDebt
 	router.GET("/debts/:debtId", api.GetDebt)
+
+	// swagger:route PUT /debts/:debtId debts EditDebt
 	router.PUT("/debts/:debtId", api.EditDebt)
+
+	// swagger:route DELETE /debts/:debtId debts DeleteDebt
 	router.DELETE("/debts/:debtId", api.DeleteDebt)
 
+	// swagger:route PUT /debts/:debtId/samples samples UpdateSample
 	router.PUT("/debts/:debtId/samples", api.UpdateSample)
+
+	// swagger:route DELETE /debts/:debtId/samples/:date samples DeleteSample
 	router.DELETE("/debts/:debtId/samples/:date", api.DeleteSample)
 
+	// swagger:route GET /values/:date values GetValuesDate
 	router.GET("/values/:date", api.GetValuesDate)
 
 	router.Use(static.Serve("/", static.LocalFile("./swagger-ui", false)))
